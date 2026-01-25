0308520b1b82f9e4f3f8d91629ba3bcf3f12aa50
Rakesh C P <rakesh.c.p@accenture.com>
2026-01-25 21:07:10 +0530

    Refractor codebase and Modified database conn

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.config.settings import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 import urllib.parse
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME") 
 
 DB_PASSWORD = urllib.parse.quote_plus(DB_PASSWORD)
 DATABASE_URL = (
